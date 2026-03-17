613af0c98c194f48bb19c7b8b0e284f389414024
Joshua Farinmade <joshuafarinmade@gmail.com>
2026-03-17 23:31:19 +0100

    End of project

--- die_visual.py
+++ die_visual.py
@@ -7,16 +7,18 @@ pio.renderers.default = "browser"
 
 
 # create a D6
-die= Die()
+die_1= Die()
+die_2 =Die()
 # Make some rolls, and store results in a list
 results= []
 for roll_num in range(1000):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
 # Analyze the results.
 frequencies= []
-pos_results = range(1, die.num_sides+1)
+max_results = die_1.num_sides + die_2.num_sides
+pos_results = range(2,max_results+1)
 for value in pos_results:
     frequency = results.count(value)
     frequencies.append(frequency)
@@ -25,7 +27,7 @@ for value in pos_results:
 #
 # print(results)
 # Visualize the results
-title= "Results of Rolling One D6 1,000 Times"
+title= "Results of Rolling Two D6 1,000 Times"
 labels= {'x': "Results", "y": "Frequency of results"}
 fig = px.bar(x=pos_results, y= frequencies, title=title, labels=labels)
 fig.show(renderer="browser")
